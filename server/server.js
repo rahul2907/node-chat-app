f543bb53cfcfb95d27947cf12d954d333a5cd355
Rahul <prahul907@gmail.com>
2019-01-07 01:09:45 +0530

    some test

--- server/server.js
+++ server/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 var publicPath = path.join(__dirname,'../public');
 
 //console.log(publicPath);
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(publicPath));
 
 app.listen(port, ()=>{
-    console.log('server up on port 3000');
-})
+    console.log(`server up on port ${port}`);
+});
