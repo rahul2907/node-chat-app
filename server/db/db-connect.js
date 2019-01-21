977b19880ca6245a57fc106c3b75c5aad3c0a142
Rahul <prahul907@gmail.com>
2019-01-21 06:07:49 +0530

    mlab config

--- server/db/db-connect.js
+++ server/db/db-connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-var url = 'mongodb://localhost:27017/chatdata' || process.env.MONGOLAB_URI;
+var url =  process.env.MONGOLAB_URI;
 mongoose.connect(url, (err)=>{
     if(err){
         return console.log('cannnot connect to db: ',err);
